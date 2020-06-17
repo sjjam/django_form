1e0798b3186f338e004008ac043ca15c7d187602
sjjam <shimjm21@gmail.com>
2020-06-17 17:29:25 +0900

    06/17 forms, ModelForm

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'articles',
     'pages',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mysite', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
     path('loop/', views.loop),
     path('throw/', views.throw),
     path('catch/', views.catch),
